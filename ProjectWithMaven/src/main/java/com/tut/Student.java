91079e3fd42b34fd1b84c31886bcab8dc3b5b4f4
parvez Ahmad <parvezi9ahmad@gmail.com>
2023-02-02 15:30:50 +0530

    one to one

--- ProjectWithMaven/src/main/java/com/tut/Student.java
+++ ProjectWithMaven/src/main/java/com/tut/Student.java
@@ -6,39 +6,59 @@ import javax.persistence.Table;
 
 @Entity()
 public class Student {
-	
+
 	@Id
 	private int id;
 	private String name;
 	private String city;
-	public Student(int id, String name, String city) {
+
+	private Certificate certi;
+
+	public Student(int id, String name, String city, Certificate certi) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.city = city;
+		this.certi = certi;
 	}
+
+	public Certificate getCerti() {
+		return certi;
+	}
+
+	public void setCerti(Certificate certi) {
+		this.certi = certi;
+	}
+
 	public Student() {
 		super();
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	@Override
 	public String toString() {
-		return "Student [id=" + id + ", name=" + name + ", city=" + city + "]";
+		return "Student [id=" + id + ", name=" + name + ", city=" + city + ", certi=" + certi + "]";
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
